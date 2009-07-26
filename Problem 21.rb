19865c8c9757dc2d0768fca2d50ef1a04073b985
Steven Littiebrant <Groxx@Navi.local>
2009-07-26 16:48:03 -0500

    solved 21 and 22, and modified supporting 22 file

---
+++ Problem 21.rb
@@ -0,0 +1,35 @@
+=begin
+  Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+  If d(a) = b and d(b) = a, where a  b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+  For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+  Evaluate the sum of all the amicable numbers under 10000.
+=end
+
+@result = []
+
+def sum_of_divisors(num)
+  result = []
+  (1..(num/2)).each do |d|
+    result << d if (num.to_f/d.to_f)%1 == 0
+  end
+  sum = 0
+  result.each do |i|
+    sum += i
+  end
+  sum
+end
+
+(1..9999).each do |i|
+  sum = sum_of_divisors(i)
+  if i == sum_of_divisors(sum) and i != sum
+    puts "appending: #{i} => #{sum}"
+    puts "  #{sum_of_divisors(i)} == #{sum_of_divisors(sum)}"
+    @result << i
+  end
+end
+puts @result.inspect
+sum = 0
+@result.each{|i| sum += i}
+puts sum
---
+++ Problem 22.rb
@@ -0,0 +1,29 @@
+@array
+
+def readToArray(filename="")
+  @array = []
+  File.new(filename).readlines.each do |line|
+    @array += line.split(" ")
+  end
+end
+
+def get_name_score(index)
+  score = 0
+  @array[index].size.times do |i|
+    score += (@array[index][i] - 64)
+  end
+  puts "#{@array[index]} == #{score} => #{score*(index+1)}"
+  return score*(index+1)
+end
+
+readToArray("Problem 22 - names.txt")
+@array.sort!
+
+score = 0
+(@array.size).times do |i|
+  score += get_name_score(i)
+end
+puts score
+
+#note: modified names.txt to use blank space as a delimiter, rather than write code to handle quoted-CSV data.
+#  I believe this is in spirit with the challenge, as this is part of my solution method.
